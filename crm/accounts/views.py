8d0776e242023a03ad35ed49556bb7f1b2974066
Robert N <rbrtmt100@gmail.com>
2020-07-26 14:33:54 +0300

    add dynamic url routes and templates

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -15,8 +15,14 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+    customer=Customer.objects.get(id=pk_test)
+    orders=customer.order_set.all()
+    order_count=orders.count()
+    context={'customer':customer, 'orders':orders, 'order_count': order_count}
+
+
+    return render(request, 'accounts/customer.html',context)
 
 
 def products(request):
